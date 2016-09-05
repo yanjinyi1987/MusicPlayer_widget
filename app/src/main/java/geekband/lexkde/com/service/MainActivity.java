21751f06dd8265d53842a58c45aff18a0970c89a
lexkde@aliyun.com <lexkde@lexkde-ThinkPad-X230>
2016-09-05 17:08:48 +0800

    update zhoubian

--- app/src/main/java/geekband/lexkde/com/service/MainActivity.java
+++ app/src/main/java/geekband/lexkde/com/service/MainActivity.java
@@ -1,13 +1,8 @@
 package geekband.lexkde.com.service;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import geekband.lexkde.com.service.service.MusicPlayerService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mButton = (Button) this.findViewById(R.id.buttonPlayMusic);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"没有什么好显示的！",Toast.LENGTH_SHORT).show();
-            }
-        });
+        setContentView(R.layout.activity_music_button);
     }
 }
